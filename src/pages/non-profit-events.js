f7d34545b9539f9ba276e3f9bea8b235f9209f90
Anliner Leona <anliner@webriq.services>
2019-03-21 09:28:54 +0800

    remove dummy

--- src/pages/non-profit-events.js
+++ src/pages/non-profit-events.js
@@ -15,8 +15,6 @@ class nonProfitPage extends React.Component {
         <SEO title={siteTitle} description={siteDescription} />
         <Container>
           <p>We are proud to offer Random axe of kindness, an initiative where we partner with select non-profits and donate 50% of proceeds to them, to assist in empowering those around us to accomplish everything they want to in life on this earth. Together we can all make a difference, make an impact, and if we can take part in changing 1 life, we have fulfilled our goal with this initiative.</p>
-          <p className="text-danger">Pics 12, 13 (Downloadable flyer, pic 13)</p>
-
         </Container>
       </Layout>
     )
