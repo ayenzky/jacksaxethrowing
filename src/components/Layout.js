1f61dad6e72979e271d2f8be1585e13c49229b2b
Anliner Leona <anliner@webriq.services>
2019-04-09 10:25:45 +0800

    add script to the layout js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,38 +1,41 @@
-import React from 'react'
-import { Container, Row, Col} from "react-bootstrap";
-import Header from './Header'
-import Footer from './Footer'
-import Hero from './Hero'
-import 'bootstrap/dist/css/bootstrap.css'
-import './layout.css'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Header from "./Header";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import "bootstrap/dist/css/bootstrap.css";
+import "./layout.css";
 
 class Layout extends React.Component {
   componentDidMount() {
-   if (window) {
-     const script = document.createElement('script')
-     script.type = 'text/javascript'
-     script.src = `https://forms.webriq.com/js/initForms`
-     const headScript = document.getElementsByTagName('script')[0]
-     headScript.parentNode.insertBefore(script, headScript)
+    if (window) {
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = `https://forms.webriq.com/js/initForms`;
+      const headScript = document.getElementsByTagName("script")[0];
+      headScript.parentNode.insertBefore(script, headScript);
 
-     const fscript = document.createElement("script");
-     fscript.src= `https://fareharbor.com/embeds/api/v1/?autolightframe=yes`
-     document.body.appendChild(fscript);
-   }
- }
+      const linkScript = document.createElement("script");
+      linkScript.id = "mcjs";
+      linkScript.src = `https://chimpstatic.com/mcjs-connected/js/users/137087abec66451b299f0ae53/cb377c9a3d7cdc590a6f1c0e5.js`;
+      document.body.appendChild(linkScript);
+
+      const fscript = document.createElement("script");
+      fscript.src = `https://fareharbor.com/embeds/api/v1/?autolightframe=yes`;
+      document.body.appendChild(fscript);
+    }
+  }
   render() {
-    const { location, title, children, customclass } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let herobanner
+    const { location, title, children, customclass } = this.props;
+    const rootPath = `${__PATH_PREFIX__}/`;
+    let herobanner;
 
     if (location.pathname === rootPath) {
-      herobanner = (
-        <Hero/>
-      )
+      herobanner = <Hero />;
     } else {
       herobanner = (
         <div className="hero-main">
-          <div className="hero-main-bg"/>
+          <div className="hero-main-bg" />
           <Container>
             <Row>
               <Col lg={8} md={9}>
@@ -41,19 +44,21 @@ class Layout extends React.Component {
             </Row>
           </Container>
         </div>
-      )
+      );
     }
     return (
-      <div className={location.pathname === rootPath ? 'home' : customclass || 'page'}>
-        <Header/>
+      <div
+        className={
+          location.pathname === rootPath ? "home" : customclass || "page"
+        }
+      >
+        <Header />
         {herobanner}
-        <main>
-          {children}
-        </main>
-        <Footer/>
+        <main>{children}</main>
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default Layout
+export default Layout;
