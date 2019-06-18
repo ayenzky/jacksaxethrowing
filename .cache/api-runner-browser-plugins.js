e6220a158ffb2acb41c03eccbdcfc3e955ba7f89
Mark Gaudiano <markgaudiano@Marks-iMac.local>
2019-06-18 16:11:01 +0800

    book online page

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('/Users/markgaudiano/Desktop/webriq/webriqpage-jacksaxethrowing/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/markgaudiano/Desktop/webriq/webriqpage-jacksaxethrowing/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/markgaudiano/Desktop/webriq/webriqpage-jacksaxethrowing/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
+    },{
+      plugin: require('/Users/markgaudiano/Desktop/webriq/webriqpage-jacksaxethrowing/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
